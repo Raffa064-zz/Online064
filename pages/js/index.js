1116c27869dc0d13f34bf75d48f0d7856d72ec2d
raffaddtg@gmail.com <raffaddtg@gmail.com>
2022-12-06 10:14:56 -0300

    Interface updates: added error hightlights on input

--- pages/js/index.js
+++ pages/js/index.js
@@ -1,4 +1,18 @@
+const input = document.querySelector('#nick')
+
+function checkValidNick() {
+    var regex = /^[a-zA-Z0-9_]{4,20}$/
+    if (input.value.match(regex)) {
+        if (input.classList.contains('error')) {
+            input.classList.remove('error')
+        }
+    } else {
+        if (!input.classList.contains('error')) {
+            input.classList.add('error')
+        }
+    }
+}
+
 function play() {
-    const input = document.querySelector('#nick')
     window.location.replace("game.html?nick=" + input.value)
 }
